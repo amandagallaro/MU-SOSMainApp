3c3c19dd207937d276287584885554c330b86a55
Kayleigh Rucinski <s1278573@monmouth.edu>
2022-11-07 14:07:17 -0500

    Initial Commit

---
+++ MU-SOSMainApp/TermsAndConditionsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TermsAndConditionsViewController.swift
+//  MU-SOSMainApp
+//
+//  Created by Kayleigh F. Rucinski on 10/27/22.
+//
+
+import Foundation
---
+++ MU-SOSMainAppUITests/MU_SOSMainAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MU_SOSMainAppUITestsLaunchTests.swift
+//  MU-SOSMainAppUITests
+//
+//  Created by Kayleigh F. Rucinski on 10/24/22.
+//
+
+import XCTest
+
+final class MU_SOSMainAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
